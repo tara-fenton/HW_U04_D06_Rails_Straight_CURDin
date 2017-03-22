0e8fb4a2d0b358c42cf23cabaa63e6a7d6ea53e8
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-03-22 12:29:20 -0400

    hw

--- straight_curdin/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "cheeses#index"
-  get "/cheeses", to: "cheeses#index" # cheeses_path
-  get "/cheeses/new", to: "cheeses#new", as: :new_cheese
-  get "/cheeses/:id", to: "cheeses#show", as: :turtle
-end
