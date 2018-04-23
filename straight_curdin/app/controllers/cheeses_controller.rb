bf186005850ea14300f4071639e6adf64a5a97f4
Tara Fenton <tarafenton@ool-ad0200b8.dyn.optonline.net>
2018-04-23 16:53:59 -0400

    bin/rails generate controller Cheeses index

---
+++ straight_curdin/app/controllers/cheeses_controller.rb
@@ -0,0 +1,5 @@
+class CheesesController < ApplicationController
+  def index
+    @cheeses = Cheese.all
+  end
+end
--- straight_curdin/config/routes.rb
+++ straight_curdin/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cheeses/index'
+  # get 'cheese/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', :to => 'cheeses#index'
   resources :cheeses
