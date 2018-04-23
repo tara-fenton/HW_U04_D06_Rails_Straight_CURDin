80713713892bcfda8f8499f2ff673fc301b0299c
Tara Fenton <tarafenton@ool-ad0200b8.dyn.optonline.net>
2018-04-23 16:32:50 -0400

    migrate cheese columns

--- straight_curdin/db/migrate/20180423202537_create_cheeses.rb
+++ straight_curdin/db/migrate/20180423202537_create_cheeses.rb
@@ -1,6 +1,10 @@
 class CreateCheeses < ActiveRecord::Migration[5.2]
   def change
     create_table :cheeses do |t|
+      t.string :name
+      t.string :milk_type
+      t.string :image_url
+      t.string :description
 
       t.timestamps
     end
