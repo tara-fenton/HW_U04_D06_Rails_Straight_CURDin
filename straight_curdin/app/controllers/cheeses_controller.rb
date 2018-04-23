056134ebfbb3a33236cd3e05f9689e9ce85f2900
Tara Fenton <tarafenton@ool-ad0200b8.dyn.optonline.net>
2018-04-23 17:35:27 -0400

    Putting the C in CURD

--- straight_curdin/app/controllers/cheeses_controller.rb
+++ straight_curdin/app/controllers/cheeses_controller.rb
@@ -6,4 +6,44 @@ class CheesesController < ApplicationController
   def show
     @cheese = Cheese.find(params[:id])
   end
+
+  def new
+    @cheese = Cheese.new
+  end
+
+  def edit
+    @cheese = Cheese.find(params[:id])
+  end
+
+  def create
+    @cheese = Cheese.new(cheese_params)
+
+    if @cheese.save
+      redirect_to cheeses_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @cheese = Cheese.find(params[:id])
+
+    if @cheese.update(cheese_params)
+      redirect_to cheeses_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cheese = Cheese.find(params[:id])
+    @cheese.destroy
+
+    redirect_to cheeses_path
+  end
+
+  private
+  def cheese_params
+    params.require(:cheese).permit(:name, :milk_type, :image_url, :description)
+  end
 end
