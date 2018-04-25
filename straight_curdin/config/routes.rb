adc552c8c68d6fcffd999b897848dba2cea10d7f
Tara Fenton <tarafenton@Taras-MBP-2.fios-router.home>
2018-04-24 21:13:10 -0400

    A signed in user can see their profile page, which includes all of their info.

--- straight_curdin/config/routes.rb
+++ straight_curdin/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'cheeses/index'
+  get 'profiles/index'
   # get 'cheese/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', :to => 'cheeses#index'
